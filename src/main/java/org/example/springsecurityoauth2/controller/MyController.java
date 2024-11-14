4686d23836e541b778140bee4b90888c171bf126
Jealyang <jaelyangchoi@gmail.com>
2024-11-14 21:53:15 +0900

    feat: "기본 컨트롤러, 페이지 생성"

---
+++ src/main/java/org/example/springsecurityoauth2/controller/MyController.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityoauth2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/my")
+    public String myPage() {
+        return "my";
+    }
+}
