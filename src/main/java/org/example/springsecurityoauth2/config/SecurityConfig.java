04b6835626d33681aa5650a0ec44deb6a38c0182
Jealyang <jaelyangchoi@gmail.com>
2024-11-16 14:49:29 +0900

    feat: "Access Token 정보 DB에 저장"

--- src/main/java/org/example/springsecurityoauth2/config/SecurityConfig.java
+++ src/main/java/org/example/springsecurityoauth2/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package org.example.springsecurityoauth2.config;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springsecurityoauth2.oauth2.CustomClientRegistrationRepo;
+import org.example.springsecurityoauth2.oauth2.CustomOAuth2AuthorizedClientService;
 import org.example.springsecurityoauth2.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +19,9 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomClientRegistrationRepo clientRegistrationRepo;
+    private final CustomOAuth2AuthorizedClientService authorizedClientService;
+    private final JdbcTemplate jdbcTemplate;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,6 +30,8 @@ public class SecurityConfig {
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
                         .clientRegistrationRepository(clientRegistrationRepo.clientRegistrationRepository())
+                        .authorizedClientService(authorizedClientService
+                                .oAuth2AuthorizedClientService(jdbcTemplate, clientRegistrationRepo.clientRegistrationRepository()))
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService))
                 );
